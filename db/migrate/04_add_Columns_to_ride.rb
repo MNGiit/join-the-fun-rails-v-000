0bc7a89820d4497c313a2525c48438f3f4a29f9f
Mohammed Noori <kmanoorib@gmail.com>
2019-07-02 22:37:57 +0000

    Automatically backed up by Learn

--- db/migrate/04_add_Columns_to_ride.rb
+++ db/migrate/04_add_Columns_to_ride.rb
@@ -1,4 +1,4 @@
-class AddColumnsToRide < ActiveRecord::Migration
+class AddColumnsToRides < ActiveRecord::Migration
   def change
     add_column :rides, :taxi_id, :integer
     add_column :rides, :passenger_id, :integer
