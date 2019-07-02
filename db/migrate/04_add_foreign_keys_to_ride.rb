e721d71da339fa04738bb9c34915a6a9e61f5fe2
Mohammed Noori <kmanoorib@gmail.com>
2019-07-02 22:31:05 +0000

    Automatically backed up by Learn

--- db/migrate/04_add_foreign_keys_to_ride.rb
+++ db/migrate/04_add_foreign_keys_to_ride.rb
@@ -1,4 +1,4 @@
-class AddForeignKeysToRide < ActiveRecord::Migration
+class AddColumnsToRide < ActiveRecord::Migration
   def change
     add_column :rides, :taxi_id, :integer
     add_column :rides, :passenger_id, :integer
